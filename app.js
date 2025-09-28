d4070158b47f044422a6d7c2b4ad924ca18c0092
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-09-28 17:34:00 +0900

    BRR: 26-27 darslar to'liq bajarildi, xatolar tuzatildi

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const fs = require("fs");
 // MongoDB chaqirish
 
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
@@ -34,11 +35,23 @@ app.post("/create-item", (req, res) => {
     console.log("user entered /create-item");
 
     const new_reja = req.body.reja;
-    console.log(new_reja);
+  
    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-    console.log(data.ops);
+   
     res.json(data.ops[0]);
    });
+});
+
+
+app.post("/delete-item",  (req, res) => {
+    const id = req.body.id;
+    db.collection("plans")
+    .deleteOne({_id: new mongodb.ObjectId(id)},
+function (err, data) {
+    res.json({ state: "success"});
+}
+);
+  
 });
 
 app.get('/author', (req, res) => {
--- public/browser.js
+++ public/browser.js
@@ -19,14 +19,14 @@ function itemTempLate(item) {
     `
 }
 
-let createField = document.get.getElementById("create-field");
+let createField = document.getElementById("create-field");
 
-document.getElementById("create-from")
+document.getElementById("create-form")
 .addEventListener("submit", function (e) {
 e.preventDefault();
 
 axios
-.post("create-item", { reja: createField.value })
+.post("/create-item", { reja: createField.value })
 .then((response) => {
    document
    .getElementById("item-list")
@@ -37,4 +37,27 @@ axios
 .catch((err) => {
     console.log("Iltimos qaytadan harakat qiling!");
 });
+});
+
+document.addEventListener("click", function(e) {
+    // delete oper
+   
+    if(e.target.classList.contains("delete-me")) {
+       if(confirm("Aniq o'chirmoqchimisiz?")) {
+        axios
+        .post("/delete-item", {id: e.target.getAttribute("data-id")})
+        .then((respose) => {
+            console.log(respose.data);
+            e.target.parentElement.parentElement.remove()
+        })
+        .catch((err) => {
+            console.log("Iltimos qaytadan harakat qiling!");
+        });
+       }
+    }
+
+    // edit oper
+    if(e.target.classList.contains("edit-me")) {
+        alert("Siz edit tugmasini bosdingiz!");
+    }
 });
