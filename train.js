267c4ad78c222b924ad43ba11361f5b74214224f
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-09-30 22:54:41 +0900

    BRR: TASK - D bajarildi

--- train.js
+++ train.js
@@ -176,3 +176,18 @@
 // shop1.sotildi(13);
 // shop1.qabul(26);
 // shop1.qoldiq("21:50");
+
+
+
+
+// TASK - D !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+function test(soz1 = "Abdulloh", soz2 = "Abdulloh") {
+  return soz1.split('').sort().join('') === soz2.split('').sort().join('');
+}
+
+console.log(test()); 
+
+// split('') --> stingni harflarga bo'lyabti
+// sort() --> harflarni alfabit bo'yicha tartibga solyabti
+// join('') --> bo'lingan harflarni yana stirnga qaytaryabti
